1b2789dad4d0f85c9b120b35e125b57ae4506bbc
Vgalassi <viniciusgalassi@gmail.com>
2023-05-08 23:16:06 -0300

    add forcas e select nós

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let nós = [];
 let componente_selecionado = document.getElementById('nósdiv');
+let forca_id = 0;
 
 function trocar(id){
     let new_display = document.getElementById(id);
@@ -28,21 +29,73 @@ function add_nó(){
         }
     }
 
-        let new_nó = {
-            nome: nome_nó.value,
-            x: Number(nó_x.value),
-            y: Number(nó_y.value)
-        }
+    let forcas = []
+    let new_nó = {
+        nome: nome_nó.value,
+        x: Number(nó_x.value),
+        y: Number(nó_y.value),
+        forcas: forcas
+    }
 
-        nós.push(new_nó);
-        let nós_display = document.getElementById('nós_display');
-        let display_newnó = document.createElement('p');
-        display_newnó.innerText = `${nome_nó.value} (${nó_x.value},${nó_y.value})`;
-        nós_display.appendChild(display_newnó);
-
-        nome_nó.value = null;
-        nó_x.value = null;
-        nó_y.value = null;
+    nós.push(new_nó);
+    let nós_display = document.getElementById('nós_display');
+    let display_newnó = document.createElement('p');
+    let option_nó = document.createElement('option');
+    let nó_seletores = document.getElementsByClassName('nó_seletor');
     
+    display_newnó.setAttribute('id',nome_nó.value);
+    display_newnó.innerText = `${nome_nó.value} (${nó_x.value},${nó_y.value})`;
+    option_nó.innerText = nome_nó.value;
+    option_nó.value = nome_nó.value;
+
+    nós_display.appendChild(display_newnó);
+    nó_seletores[0].appendChild(option_nó);
+    for(let i= 1;i<nó_seletores.length;i++){
+    nó_seletores[i].appendChild(option_nó.cloneNode(true));
+    }
+
+    nome_nó.value = null;
+    nó_x.value = null;
+    nó_y.value = null;
+
 }
 
+function find_nó(nome){
+    for(let i = 0;i<nós.length;i++){
+        if(nós[i].nome == nome){
+            return i;
+        }
+    }
+    return false
+}
+
+function add_forca(){
+    let nó_selecionado = document.getElementById('nó_seletor_forca').value;
+    if(nó_selecionado == ''){
+        return window.alert('Selecione um nó');
+    }
+    let i = find_nó(nó_selecionado);
+    componentex = document.getElementById('força_x').value;
+    componentey = document.getElementById('força_y').value;
+
+    if(componentex == '' || componentey == ''){
+        return window.alert('Preencha todos os valores');
+    }
+
+    componentex = Number(componentex);
+    componentey = Number(componentey);
+
+    let new_forca = {
+        id: forca_id,
+        x: componentex,
+        y: componentey
+    }
+    forca_id++;
+    nós[i].forcas.push(new_forca);
+    let new_forca_display = document.createElement('p');
+    let p_nó = document.getElementById(nós[i].nome);
+    new_forca_display.innerText = ` F${new_forca.id}: (${new_forca.x},${new_forca.x})`;
+    p_nó.appendChild(new_forca_display);
+
+    
+}
