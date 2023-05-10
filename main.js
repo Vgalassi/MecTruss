5d02bc4a13b4790d970a941e9b0fcd57283d449b
Vgalassi <viniciusgalassi@gmail.com>
2023-05-10 17:47:01 -0300

    add membros e interface gráfica

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 let nós = [];
 let componente_selecionado = document.getElementById('nósdiv');
 let forca_id = 0;
+let grafico = document.getElementById('grafico');
+let gtx = grafico.getContext('2d');
+
+
+
 
 function trocar(id){
     let new_display = document.getElementById(id);
@@ -36,6 +41,18 @@ function add_nó(){
         y: Number(nó_y.value),
         forcas: forcas
     }
+
+    gtx.beginPath();
+    gtx.arc(nó_x.value*40+40,400-nó_y.value*20,3,0,2 * Math.PI);
+    gtx.strokeStyle = '#00000';
+    gtx.fill();
+    gtx.stroke();
+
+    gtx.font = "10px Arial";
+    gtx.textAlign = "center";
+    gtx.fillText(nome_nó.value,nó_x.value*40+40,390-nó_y.value*20);
+   
+
 
     nós.push(new_nó);
     let nós_display = document.getElementById('nós_display');
@@ -74,13 +91,13 @@ function add_forca(){
     if(nó_selecionado == ''){
         return window.alert('Selecione um nó');
     }
-    let i = find_nó(nó_selecionado);
     componentex = document.getElementById('força_x').value;
     componentey = document.getElementById('força_y').value;
-
     if(componentex == '' || componentey == ''){
         return window.alert('Preencha todos os valores');
     }
+    let i = find_nó(nó_selecionado);
+
 
     componentex = Number(componentex);
     componentey = Number(componentey);
@@ -90,12 +107,97 @@ function add_forca(){
         x: componentex,
         y: componentey
     }
+
+
     forca_id++;
+
+    let coordenadasx = nós[i].x;
+    let coordenadasy = nós[i].y;
+    if(new_forca.x>0){
+        coordenadasx += 1;
+    }else if(new_forca.x<0){
+        coordenadasx -= 1;
+    }
+    if(new_forca.y>0){
+        coordenadasy += 2;
+    }else if(new_forca.y<0){
+        coordenadasy -= 2;
+    }
+    coordenadasx = coordenadasx * 40 + 40;
+    coordenadasy = 400 - coordenadasy*20
+    
+    gtx.beginPath();
+    gtx.moveTo(nós[i].x*40+40,400 - nós[i].y*20);
+    gtx.lineTo(coordenadasx,coordenadasy);
+    const angle = Math.atan2(coordenadasy - (400 - nós[i].y*20), coordenadasx - (nós[i].x*40+40));
+    gtx.lineTo(coordenadasx - 10 * Math.cos(angle - Math.PI / 6), coordenadasy - 10 * Math.sin(angle - Math.PI / 6));
+    gtx.moveTo(coordenadasx, coordenadasy);
+    gtx.lineTo(coordenadasx - 10 * Math.cos(angle + Math.PI / 6), coordenadasy - 10 * Math.sin(angle + Math.PI / 6));
+    gtx.strokeStyle = '#E60d11';
+    gtx.stroke();
+
     nós[i].forcas.push(new_forca);
     let new_forca_display = document.createElement('p');
     let p_nó = document.getElementById(nós[i].nome);
-    new_forca_display.innerText = ` F${new_forca.id}: (${new_forca.x},${new_forca.x})`;
+    new_forca_display.innerText = ` F${new_forca.id}: (${new_forca.x},${new_forca.y})`;
     p_nó.appendChild(new_forca_display);
 
+    document.getElementById('força_x').value = null;
+    document.getElementById('força_y').value = null;
+
     
+} 
+
+function add_membro(){
+    let nó_selecionado1 = document.getElementById('nó_seletor_membro1').value;
+    let nó_selecionado2 = document.getElementById('nó_seletor_membro2').value;
+    if(nó_selecionado1 == '' || nó_selecionado2 == ''){
+        return window.alert('Selecione todos os nós');
+    }
+    if(nó_selecionado1 == nó_selecionado2){
+        return window.alert('Selecione nós diferentes');
+    }
+
+    let i = find_nó(nó_selecionado1);
+    let j = find_nó(nó_selecionado2);
+    let componentex;
+    let componentey;
+    let nome_membro = nós[i].nome + nós[j].nome;
+
+
+
+    
+    if(nós[i].x == nós[j].x){
+        componentex = undefined;
+        componentey = 0;
+    }else if(nós[i].y == nós[j].y){
+        componentex = 0;
+        componentey = undefined;
+    }else{
+        componentex = undefined;
+        componentey = undefined;
+    }
+
+    let new_membro = {
+        nome: nome_membro,
+        x: componentex,
+        y: componentey
+    }
+
+    gtx.moveTo(nós[i].x*40+40,400 - nós[i].y*20);
+    gtx.lineTo(nós[j].x*40+40,400 - nós[j].y*20);
+    gtx.stroke();
+
+
+    nós[i].forcas.push(new_membro);
+    nós[j].forcas.push(new_membro);
+    let new_membro_display = document.createElement('p');
+    let p_nó = document.getElementById(nós[i].nome);
+    let p_nó2 = document.getElementById(nós[j].nome);
+    new_membro_display.innerText = `Membro ${new_membro.nome}`;
+    p_nó.appendChild(new_membro_display);
+    p_nó2.appendChild(new_membro_display.cloneNode(true));
+
+
+
 }
